3a60c278e4da3527b3a53d12191ff00eea41fce4
blackboy01 <494450261@qq.com>
2016-05-12 00:34:56 +0800

    Create XujfTest.py

---
+++ XujfTest.py
@@ -0,0 +1,10 @@
+# - * - coding: utf-8 - *- 
+# CopyRight(C) Xujf@2016
+# Descripthon: Just for Test
+
+def main():
+    print( " I just want to try to add one file... " )
+    print( " Hello World" )
+
+if __name__ == "__main__":
+    main()
